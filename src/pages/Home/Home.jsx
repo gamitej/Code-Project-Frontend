e69c866d78b6e420e53da0f688503ff58630014d
gamitej <gamitejpratapsingh998@gmail.com>
2023-07-09 11:55:46 +0530

    done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
 
     setTimeout(() => {
       cardElement.classList.remove("highlighted");
-    }, 3000);
+    }, 2000);
   };
 
   // =================== CALL ALL PINS API ========================
